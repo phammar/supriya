afda8999f366793672da67af78d2ff0e9262baf8
Peter Hammar <peter@hammar.ch>
2025-03-27 14:24:38 +0100

    reformat with docstrfmt

--- docs/source/MI-ugens.rst
+++ docs/source/MI-ugens.rst
@@ -1,26 +1,27 @@
 Mutable Instruments UGens
-=========
+=========================
 
 Source
---------
+------
 
 Source and binaries available here: https://github.com/v7b1/mi-UGens
 
 Installation
---------
+------------
 
 Instructions adapted from https://tidalcycles.org/docs/reference/mi-ugens-installation/
 
-- Put the mi-UGens directory extracted from the download binaries in your in Extensions dir (`Platform.userExtensionDir`)
+- Put the mi-UGens directory extracted from the download binaries in your in Extensions
+  dir (`Platform.userExtensionDir`)
+
 Example: ~/.local/share/SuperCollider/Extensions
 
 - Create a new synthdef file mi-ugens.scd, with these synthdefs
-  
+
 Linux: ~/.local/share/SuperCollider/synthdefs/mi-ugens.scd
 
-Configure SuperCollider - edit your startup.scd:
-Linux: ~/.conf/SuperCollider/startup.scd
+Configure SuperCollider - edit your startup.scd: Linux:
+~/.conf/SuperCollider/startup.scd
 
 Load the mi-ugens.scd synthdef in startup.scd.
 load("~/.local/share/SuperCollider/synthdefs/mi-ugens.scd");
-
--- supriya/ugens/local.py
+++ supriya/ugens/local.py
@@ -2,55 +2,61 @@ from .core import UGen, param, ugen
 
 @ugen(ar=True, is_multichannel=True, channel_count=2)
 class MiPlaits(UGen):
-    """
-    Mutable Instruments Plaits UGen.
+    """Mutable Instruments Plaits UGen.
 
     Attributes
-    ----------
-    pitch : int
+    ==========
+
+    pitch
         pitch (midi note)
 
-    engine : int
-        chooses synthesis engine (0 -- 15):
-        0:virtual_analog_engine, 1:waveshaping_engine, 2:fm_engine, 3:grain_engine, 4:additive_engine, 5:wavetable_engine, 6:chord_engine, 7:speech_engine, 8:swarm_engine, 9:noise_engine, 10:particle_engine, 11:string_engine, 12:modal_engine, 13:bass_drum_engine, 14:snare_drum_engine, 15:hi_hat_engine
+    engine
+        chooses synthesis engine (0 -- 15): 0:virtual_analog_engine,
+        1:waveshaping_engine, 2:fm_engine, 3:grain_engine, 4:additive_engine,
+        5:wavetable_engine, 6:chord_engine, 7:speech_engine, 8:swarm_engine,
+        9:noise_engine, 10:particle_engine, 11:string_engine, 12:modal_engine,
+        13:bass_drum_engine, 14:snare_drum_engine, 15:hi_hat_engine
 
-    harm : int
+    harm
         harmonics parameter (0. -- 1.)
 
-    timbre : int
+    timbre
         timbre parameter (0. -- 1.)
 
-    morph : int
+    morph
         morph parameter (0. -- 1.)
 
-    trigger : int
+    trigger
         A non-zero value causes a trigger to
-            a) fire the internal decaying envelope generator
-            b) excites the physical and percussive models
-            c) strikes the internal low-pass gate (LPG) (unless the 'level' input is modulated (patched))
-            d) samples and holds the value of the 'model' input
+            a. fire the internal decaying envelope generator
+            b. excites the physical and percussive models
+            c. strikes the internal low-pass gate (LPG) (unless the 'level' input is
+               modulated (patched))
+            d. samples and holds the value of the 'model' input
 
+    level
+        Opens the internal low-pass gate, to simultaneously control the amplitude and
+        brightness of the output signal. Also acts as an accent control when triggering
+        the physical or percussive models.
 
-    level : int
-        Opens the internal low-pass gate, to simultaneously control the amplitude and brightness of the output signal. Also acts as an accent control when triggering the physical or percussive models.
-
-    fm_mod : int
+    fm_mod
         fm modulation amount, if internal env is activated by trigger (-1. -- 1.)
 
-    timb_mod : int
+    timb_mod
         timbre modulation amount, if internal env is activated by trigger (-1. -- 1.)
 
-    morph_mod : int
+    morph_mod
         morph modulation amount, if internal env is activated by trigger (-1. -- 1.)
 
-    decay : int
+    decay
         decay rate of internal lowpass gate (0. -- 1.)
 
-    lpg_colour : int
+    lpg_colour
         "colour" of internal lowpass gate (0. -- 1.)
 
-    mul : int
+    mul
         set output gain
+
     ::
 
         >>> supriya.ugens.MiPlaits.ar()
@@ -69,19 +75,3 @@ class MiPlaits(UGen):
     decay=param(0.5)
     lpg_colour=param(0.5)
     mul=param(1.0)
-
-# Output of
-# ugen.postln;
-# ugen.superclass.postln;
-# ugen.class.methods.do({
-#     arg method;
-#     method.postln;
-#     method.keyValuePairsFromArgs.asDict.postln;
-# });
-# MiPlaits
-# MultiOutUGen
-# Meta_MiPlaits:ar
-# IdentityDictionary[ (fm_mod -> 0.0), (trigger -> 0.0), (decay -> 0.5), (timbre -> 0.5), (mul -> 1.0), 
-#   (harm -> 0.1), (morph_mod -> 0.0), (pitch -> 60.0), (morph -> 0.5), (lpg_colour -> 0.5), 
-#   (level -> 0), (engine -> 0), (timb_mod -> 0.0) ]
-
